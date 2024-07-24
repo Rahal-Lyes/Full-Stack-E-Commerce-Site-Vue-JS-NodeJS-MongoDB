624a34799aeb84239e6c5e2a5ac9b7d83f9b25ab
Rahal-Lyes <lyes Postavo>
2024-07-24 16:28:44 +0100

    'v1.1.1'

---
+++ client/src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import ProductsPage from "../views/ProductsPage.vue";
+import ProductDetails from "../views/ProductDetails";
+import cartPage from "../views/CartPage.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/products",
+    name: "ProductsPage",
+    component: ProductsPage,
+  },
+  {
+    path: "/product/:id",
+    name: "ProductDetails",
+    component: ProductDetails,
+  },
+  {
+    path: "/cart",
+    name: "cartPage",
+    component: cartPage,
+  },
+  {
+    path: "/",
+    redirect: "/products",
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
---
+++ client/src/views/ProductDetails.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Product Details</h1>
+</template>
+<script>
+export default {
+  name: "ProductDetails",
+};
+</script>
